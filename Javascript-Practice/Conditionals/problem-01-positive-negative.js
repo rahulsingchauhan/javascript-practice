678396136b47b8e07adc838916d5d7081c55b0c9
Rahul Singh Chauhan <rahulsinghchauhanrj@gmail.com>
2025-05-26 18:26:30 +0530

    JS Condationals 01

---
+++ Javascript-Practice/Conditionals/problem-01-positive-negative.js
@@ -0,0 +1,19 @@
+//Problem 1: Check if a number is positive or negative....
+// •  "Positive" agar number 0 se bada hai.
+// •  "Negative" agar number 0 se chhota hai.
+// •  "Zero" agar number 0 ke barabar hai.
+//************************** END OF STATEMENT ****************************
+
+// let num = -3;
+
+// if (num > 0) {
+//   console.log("Positive");
+// } else if (num < 0) {
+//   console.log("Negative");
+// } else {
+//   console.log("Zero");
+// };
+
+// ------------------------------------------------ END OF CODE -----------------------------------------------
+
+
